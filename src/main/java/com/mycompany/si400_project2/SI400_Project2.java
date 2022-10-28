22d1c727df9aa8f7716363b709f4fa751411363d
Marcos César Medeiros <marcolla.hc@gmail.com>
2022-10-28 20:59:01 -0300

    feat: dependência do sqlite e camada de visualização

---
+++ src/main/java/com/mycompany/si400_project2/SI400_Project2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.si400_project2;
+
+/**
+ *
+ * @author marcos-medeiros
+ */
+public class SI400_Project2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
