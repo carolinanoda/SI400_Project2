1231a37713d720c816796c8242a30a5a8a297cbf
Marcos César Medeiros <marcolla.hc@gmail.com>
2022-11-05 01:43:16 -0300

    feat: classes de base para o projeto

---
+++ src/main/java/Model/TextLines.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author marcos-medeiros
+ */
+public class TextLines {
+    private int groupId;
+    private String file;
+    private int line;
+    private String text;
+
+    public TextLines(int groupId, String file, int line, String text) {
+        this.groupId = groupId;
+        this.file = file;
+        this.line = line;
+        this.text = text;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/Model/TextLinesDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.awt.List;
+
+/**
+ *
+ * @author marcos-medeiros
+ */
+public class TextLinesDAO {    
+    public List<String> retrieveAllLines(int groupId);
+}
